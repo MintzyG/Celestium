cb1cfa1fef41cecea0cc695da26a4ddbb350fac4
MintzyG <ericbraga2000@gmail.com>
2024-07-12 14:28:38 -0300

    Added buffer changing binds

--- config/native/keymaps.nix
+++ config/native/keymaps.nix
@@ -6,5 +6,17 @@
       options.silent = true;
       action = "<cmd>nohlsearch<CR>";
     }
+    {
+      mode = "n";
+      key = "<leader>q";
+      options.silent = true;
+      action = ":bprevious<cr>";
+    }
+    {
+      mode = "n";
+      key = "<leader>w";
+      options.silent = true;
+      action = ":bnext<cr>";
+    }
   ];
 }

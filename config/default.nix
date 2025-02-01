c8a9ca769e0fbdc487a1a5df6b3f83fe4e2a3fba
MintzyG <ericbraga2000@gmail.com>
2025-01-31 21:16:59 -0300

    Switched to evergarden theme

--- config/default.nix
+++ config/default.nix
@@ -1,9 +1,24 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./imports.nix
   ];
 
-  colorschemes.dracula.enable = true;
+  extraPlugins = with pkgs.vimUtils; [
+    (buildVimPlugin {
+      pname = "evergarden";
+      version = "1.0.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "comfysage";
+        repo = "evergarden";
+        rev = "be3527f111aaceecef08d12682d62ab5263d92c7";
+        sha256 = "sha256-8BFTBYHVtEl1rtaLCEmSCqGrFsEyd5P2N5ujEaFW9Bk=";
+      };
+    })
+  ];
+
+  colorscheme = "evergarden";
 
   globals = {
     mapleader = " ";
